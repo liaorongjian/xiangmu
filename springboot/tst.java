7a8a98c23ed798c4343148ccd1bbf6741b39560a
git <664080376@qq.com>
2018-11-24 23:13:07 +0800

    本地工程加入

---
+++ springboot/tst.java
@@ -0,0 +1,29 @@
+package com.itcast.springboot;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class tst {
+    public static void main(String[] args) {
+        Map<String,String>map = new HashMap<String, String>();
+        for (Map.Entry<String, String> stringStringEntry : map.entrySet()) {
+            String key = stringStringEntry.getKey();
+            String value = stringStringEntry.getValue();
+        }
+        for (String s : map.keySet()) {
+            System.out.println(s);
+
+        }
+        for (String s : map.values()) {
+            System.out.println(s);
+        }
+
+        Iterator<Map.Entry<String, String>> iterator = map.entrySet().iterator();
+        while (iterator.hasNext()){
+            Map.Entry<String, String> next = iterator.next();
+            System.out.println(next.getKey()+next.getValue());
+        }
+    }
+
+}
